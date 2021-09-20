93001c9971b5ef270bbe7fd8911b1b0b6dde4e93
F. G. Robertson <me@grantrobertson.com>
2021-09-20 05:33:26 -0700

    Cleanup and the beginings of packaging for pip.

---
+++ Setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Roland-Gp-8',
+      version='1.0',
+      description='Library for inspecting and modifying Roland Gp-8 sysex dump format.',
+      author='F.G. Robertson',
+      author_email='me@grantrobertson.com',
+      url='https://www.github.com/grobertson/Roland-GP-8/',
+      packages=['RolandGp8'],
+     )
